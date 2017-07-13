bb51a4fd281f6913cfd8eaaabd5222e253013f36
elson Huang <elson.huang@jamzoo.com.tw>
2017-07-13 12:54:24 +0800

    upload data use googleSheet

--- PostToSheet/ViewController.swift
+++ PostToSheet/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    let url = "https://sheetsu.com/apis/v1.0/c39dd83b8e27"
+    
+    
+    
+    @IBAction func upload(_ sender: UIButton) {
+        uploadSothing(getstring: url)
+        
+    }
+    
+    func uploadSothing(getstring:String){
+        if let getUrl = URL(string: getstring){
+            var getRequest = URLRequest(url: getUrl)
+            getRequest.httpMethod = "POST"
+           let postStr =  "name=elson&grade=99"
+             let uploadData = postStr.data(using: String.Encoding.utf8)
+           let task =  URLSession.shared.uploadTask(with: getRequest, from: uploadData, completionHandler: { (getdata, response, errorr) in
+                if errorr != nil{
+                    print(errorr?.localizedDescription)
+                }
+                if getdata != nil{
+                    do{
+                     let json = try  JSONSerialization.jsonObject(with: getdata!, options: .mutableContainers) as! NSDictionary
+                    print(json)
+                    }catch{
+                        
+                    }
+                }
+            })
+           task.resume()
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      
+        
     }
 
     override func didReceiveMemoryWarning() {
